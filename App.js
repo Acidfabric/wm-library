e0bc89abd3adb28ffbf010b93630ed15bc2d0654
Danas <danasvog@gmail.com>
2018-09-06 19:06:14 +0300

    functions for storage are created

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Font, KeepAwake } from "expo";
-import { StyleSheet } from "react-native";
+import { StyleSheet, AsyncStorage } from "react-native";
 import { StyleProvider, Spinner } from "native-base";
 import { createStackNavigator } from "react-navigation";
 
 import getTheme from "./native-base-theme/components";
 import CameraScreen from "./src/screens/Camera/Camera";
 import StartScreen from "./src/screens/Start/Start";
+
+import helper from './utils/helpers';
 
 const RootStack = createStackNavigator(
   {
@@ -20,11 +22,12 @@ const RootStack = createStackNavigator(
 
 export default class App extends React.Component {
   state = {
-    ready: false
+    ready: false,
   }
 
   async componentWillMount() {
     KeepAwake.activate();
+    
     await Font.loadAsync({
       /* eslint-disable global-require */
       Roboto: require("native-base/Fonts/Roboto.ttf"),
@@ -35,6 +38,28 @@ export default class App extends React.Component {
     this.setState({
       ready: true
     });
+  }
+
+  async componentDidMount() {
+    const response = await this.takeABookForDanas();
+    console.log(response);
+  }
+
+  async fetchUserAndBooks() {
+    const allBooks = await helper.getAllBooks();
+    const allUsers = await helper.getAllUsers();
+    
+    return [allUsers, allBooks];
+  }
+
+  async initStorage() {
+    const response = await helper.initStorage();
+    return response;
+  }
+
+  async takeABookForDanas() {
+    const response = await helper.manageBook({ bookQr: 1, faceId: 'e28c79f8-106e-44a5-948a-c8ee782a1080' });
+    return response;
   }
 
   render() {
---
+++ utils/helpers.js
@@ -0,0 +1,167 @@
+import { AsyncStorage } from 'react-native';
+
+export default {
+  async initStorage() {
+    const initBooks = [
+      {qr: 2, name: 'THE LEVITAN PITCH', isAvailable: true},
+      {qr: 1, name: 'YES TO THE MESS', isAvailable: true},
+    ];
+    const initUsers = [
+      {
+        name: 'Karolis',
+        faceId: '84246a55-910a-4665-ba5b-4a6605274cba', 
+        books:[]
+      },
+      {
+        name: 'Tomas',
+        faceId: '668fad48-7845-4b04-8abd-9a11331e263f',
+        books:[]
+      },
+      {
+        name: 'Indre',
+        faceId: '6a79a85a-adb6-45f7-a208-3179fc25dabd',
+        books:[]
+      },
+      {
+        name: 'Arminas',
+        faceId: '778249a5-83fa-4be8-bee7-498eec197bee',
+        books:[]
+      },
+      {
+        name: 'Domas',
+        faceId: 'bfc77a3d-db96-4720-bb97-809fc00e9a74',
+        books:[]
+      },
+      {
+        name: 'Danas',
+        faceId: 'e28c79f8-106e-44a5-948a-c8ee782a1080',
+        books:[]
+      },
+    ];
+  
+    try {
+      await AsyncStorage.setItem('@DanskeLibrary:Users', JSON.stringify(initUsers));
+      await AsyncStorage.setItem('@DanskeLibrary:Books', JSON.stringify(initBooks));
+
+      return { success: true };
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  async getAllBooks() {
+    try {
+      const storedBooks = await AsyncStorage.getItem('@DanskeLibrary:Books');
+      const booksArr = await JSON.parse(storedBooks);
+
+      return { books: booksArr }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  async getAllUsers() {
+    try {
+      const storedUsers = await AsyncStorage.getItem('@DanskeLibrary:Users');
+      const usersArr = await JSON.parse(storedUsers);
+
+      return { users: usersArr }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  async manageBook(request) {
+    const { faceId, bookQr} = request;
+
+    try {
+      const storedBooks = await AsyncStorage.getItem('@DanskeLibrary:Books');
+      const storedUsers = await AsyncStorage.getItem('@DanskeLibrary:Users');
+
+      const booksArr = JSON.parse(storedBooks);
+      const usersArr = JSON.parse(storedUsers);
+
+      const userFound = usersArr.find(user => { return user.faceId === faceId });
+      const bookFound = booksArr.find(book => { return book.qr === bookQr });
+      const userHasThisBook = userFound.books.find(book => { return book === bookQr });
+      
+      const userIndex = usersArr.indexOf(userFound);
+      const bookIndex = booksArr.indexOf(bookFound);
+
+      if(!userFound) {
+        return { success: false, message: 'user doesn\'t exist' }
+      }
+
+      if(!bookFound) {
+        return { success: false, message: 'this book doesn\'t exist in the library' };
+      } 
+
+      if(bookFound && bookFound.isAvailable) {
+        userFound.books.push(bookQr);
+        bookFound.isAvailable = false;
+
+        usersArr[userIndex] = userFound;
+        booksArr[bookIndex] = bookFound;
+
+        try {
+          await AsyncStorage.setItem('@DanskeLibrary:Users', JSON.stringify(usersArr));
+          await AsyncStorage.setItem('@DanskeLibrary:Books', JSON.stringify(booksArr));
+  
+          return { success: true, message: 'Congratulations ' + userFound.name + '! You have taken the book - ' + bookFound.name }
+        } catch (error) {
+          console.log(error);
+        }
+      } else if (userHasThisBook) {
+        const usersBookIndex = userFound.books.indexOf(userHasThisBook);
+
+        if (usersBookIndex !== -1) {
+          userFound.books.splice(usersBookIndex);
+          bookFound.isAvailable = true;
+        }
+
+        usersArr[userIndex] = userFound;
+        booksArr[bookIndex] = bookFound;
+
+        try {
+          await AsyncStorage.removeItem('@DanskeLibrary:Users');
+          await AsyncStorage.removeItem('@DanskeLibrary:Books');
+
+          await AsyncStorage.setItem('@DanskeLibrary:Users', JSON.stringify(usersArr));
+          await AsyncStorage.setItem('@DanskeLibrary:Books', JSON.stringify(booksArr));
+
+          return { success: true, message: 'Congratulations ' + userFound.name + '! You bring back the book - ' + bookFound.name }
+        } catch (error) {
+          console.log(error);
+        }
+
+      } else {
+        return { success: false, message: 'Book is not available' }
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // registration = async(request) => {
+  //   const { name, faceId, bookQr } = request;
+  //   const newUser = {name, faceId, books: [bookQr]};
+
+  //   try {
+  //     const storedUsers = await AsyncStorage.getItem('@DanskeLibrary:Users');
+  //     if (storedUsers) {
+  //       let existingUsers = JSON.parse(storedUsers);
+  //       existingUsers.push(newUser);
+
+  //       try {
+  //         await AsyncStorage.setItem('@DanskeLibrary:Users', JSON.stringify(existingUsers));
+
+  //         return { success: true };
+  //       } catch (error) {
+  //         console.log(error);
+  //       }
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // },
+}
